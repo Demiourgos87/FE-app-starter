a6d1bcc54708044085781cb901077cc2f7de2d6d
Bojan Knezevic <bojan.knezevic@gmail.com>
2019-07-15 16:02:25 +0200

    formatting

--- config/build.config.js
+++ config/build.config.js
@@ -9,109 +9,110 @@ const cssOutput = 'css/style.[chunkhash:8].css'
 const jsOutput = 'js/[name].[chunkhash:8].bundle.js'
 
 module.exports = {
-    entry: {
-        main: './src/main.js'
-    },
-    output: {
-        path: path.resolve(__dirname, outputDir),
-        filename: jsOutput
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { presets: ['babel-preset-env'] }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        { loader: 'css-loader' },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                config: {
-                                    path: './config/postcss.config.js'
-                                }
-                            }
-                        },
-                        'sass-loader'
-                    ],
-                    publicPath: '../'
-                })
-            },
-            {
-                test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
-                exclude: /(node_modules|img)/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/fonts/'
-                    }
-                }
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                exclude: /(node_modules|fonts)/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'assets/img/'
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
-                        },
-                    }
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        minimize: false,
-                        attrs: ['img:src', 'source:srcset', 'img:srcset']
-                    }
-                }
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'async',
-            minSize: 30000,
-            maxSize: 0,
-            minChunks: 1,
-            maxAsyncRequests: 5,
-            maxInitialRequests: 3,
-            automaticNameDelimiter: '~',
-            automaticNameMaxLength: 30,
-            name: true
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, outputDir),
+    filename: jsOutput
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
         }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new ExtractTextPlugin(cssOutput),
-        new HtmlWebpackPlugin({
-            title: 'Webpack App',
-            filename: 'index.html',
-            template: './src/index.html',
-            minify: {
-                removeAttributeQuotes: true,
-                collapseWhitespace: true
-            }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  path: './config/postcss.config.js'
+                }
+              }
+            },
+            'sass-loader'
+          ],
+          publicPath: '../'
         })
+      },
+      {
+        test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+        exclude: /(node_modules|img)/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/fonts/'
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        exclude: /(node_modules|fonts)/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/img/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: false,
+            attrs: ['img:src', 'source:srcset', 'img:srcset']
+          }
+        }
+      }
     ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      automaticNameMaxLength: 30,
+      name: true
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ExtractTextPlugin(cssOutput),
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true
+      }
+    })
+  ]
 };
--- config/dev.config.js
+++ config/dev.config.js
@@ -1,112 +1,121 @@
-const webpack = require('webpack'),
-      ExtractTextPlugin = require("extract-text-webpack-plugin"),
-      WebpackBuildNotifierPlugin = require('webpack-build-notifier'),
-      HtmlWebpackPlugin = require('html-webpack-plugin'),
-      path = require('path');
+const webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-    entry: {
-        main: './src/main.js'
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist/'),
-        filename: '[name].bundle.js'
-    },
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            // ----- JS ES2015 compiling
-            { test: /\.js$/, exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { presets: ['babel-preset-env'] }
-                }
-            },
-            // ----- SASS compiling
-            {
-                test: /\.scss$/,
-                use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        { loader: "css-loader", options: { sourceMap: true, url: false } },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: true,
-                                outputStyle: "expanded",
-                                sourceMapContents: true
-                            }
-                        }
-                    ],
-                    publicPath: '../'
-                }))
-            },
-            // ----- Font loading
-            {
-                test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
-                exclude: /(node_modules|img)/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/fonts/'
-                    }
-                }
-            },
-            // ----- Image loading
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                exclude: /(node_modules|fonts)/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/img/'
-                    }
-                }
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist/'),
+    filename: '[name].bundle.js'
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      // ----- JS ES2015 compiling
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
+      // ----- SASS compiling
+      {
+        test: /\.scss$/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [{
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                url: false
+              }
             },
             {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        minimize: false,
-                        attrs: ['img:src', 'source:srcset', 'img:srcset']
-                    }
-                }
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+                outputStyle: "expanded",
+                sourceMapContents: true
+              }
             }
-        ]
-    },
-    // ----- Webpack dev server options
-    devServer: {
-        contentBase: path.join(__dirname, '../dist/'),
-        watchContentBase: true,
-        hot: true,
-        compress: true,
-        port: 3300,
-        stats: {
-            all: false,
-            errors: true,
-            warnings: true
-        },
-        open: true
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new ExtractTextPlugin({
-            filename: 'css/style.css',
-            allChunks: true
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Webpack App',
-            filename: 'index.html',
-            template: 'src/index.html'
-        }),
-        new WebpackBuildNotifierPlugin({
-            title: "Webpack",
-            suppressSuccess: false
-        })
+          ],
+          publicPath: '../'
+        }))
+      },
+      // ----- Font loading
+      {
+        test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+        exclude: /(node_modules|img)/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/fonts/'
+          }
+        }
+      },
+      // ----- Image loading
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        exclude: /(node_modules|fonts)/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/img/'
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: false,
+            attrs: ['img:src', 'source:srcset', 'img:srcset']
+          }
+        }
+      }
     ]
+  },
+  // ----- Webpack dev server options
+  devServer: {
+    contentBase: path.join(__dirname, '../dist/'),
+    watchContentBase: true,
+    hot: true,
+    compress: true,
+    port: 3300,
+    stats: {
+      all: false,
+      errors: true,
+      warnings: true
+    },
+    open: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+      filename: 'css/style.css',
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new WebpackBuildNotifierPlugin({
+      title: "Webpack",
+      suppressSuccess: false
+    })
+  ]
 };
